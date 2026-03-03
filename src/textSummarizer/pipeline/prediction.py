4ad4e5aa07cef65fe337c99d710cd906311c7914
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-03-02 20:06:53 -0500

    updated

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -1,5 +1,6 @@
 from textSummarizer.config.configuration import ConfigurationManager
 from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
 from transformers import pipeline
 
 
@@ -10,11 +11,17 @@ class PredictionPipeline:
 
     
     def predict(self,text):
-        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        #tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
         gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_new_tokens": 150}
-        
+        #pipe = pipeline("text-generation", model=self.config.model_path,tokenizer=tokenizer)
 
-        pipe = pipeline("text-generation", model=self.config.model_path,tokenizer=tokenizer)
+
+        model_ckpt = "google/pegasus-cnn_dailymail"
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+        model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(model_ckpt).to(device)
+        pipe = pipeline("text-generation", model_pegasus,tokenizer=tokenizer)
+        
         
 
         print("Dialogue:")
